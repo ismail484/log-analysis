badb18b92ca8a5d02acd215f50fa6f01faec9c08
Ismail <Mohamed.Ahmed.Ismail@dnvgl.com>
2017-05-30 16:35:47 +0200

    change the format of the date

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@ import string
 DBNAME = "news"
 try:
   def get_articles():
-    """Return all posts from the 'database', most recent first."""
+    """Return the most popular three articles of all time from the 'database', most popular first."""
     db = psycopg2.connect(database=DBNAME)
     #print("successfully executed  step one")
     c = db.cursor()
@@ -23,7 +23,7 @@ get_articles()
 
 try:
   def get_authors():
-    """Return all posts from the 'database', most recent first."""
+    """Return the most popular article authors of all time from the 'database', most popular first."""
     db = psycopg2.connect(database=DBNAME)
     #print("successfully executed  step one")
     c = db.cursor()
@@ -42,18 +42,18 @@ get_authors()
 
 try:
   def get_date():
-    """Return all posts from the 'database', most recent first."""
+    """Return days did more than 1% of requests lead to errors from the 'database'"""
     db = psycopg2.connect(database=DBNAME)
     #print("successfully executed  step one")
     c = db.cursor()
-    c.execute("select * from error_precentage where error_precentage>1;")
+    c.execute("select to_char(date,'MonthDD,YYYY'),error_precentage from error_precentage where error_precentage>1;")
     posts = c.fetchall()
     db.close()
 
     print('--------------------------------------------')
     print('the days whcih have request error more than 1%')
     for a, b in posts:
-      print  "the date is:" ,a, " is --->", b ,"%"
+      print  "the day :" ,a, " has --->", b ,"%"
     return posts
 except:
   print("sorry it's a problem")
