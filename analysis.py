b693bd18a22e52e628c46efec5902777820937e2
Ismail <Mohamed.Ahmed.Ismail@dnvgl.com>
2017-05-30 18:49:30 +0200

    create DB function and refactor other functions

--- analysis.py
+++ analysis.py
@@ -1,63 +1,90 @@
 import psycopg2, bleach
 import string
 
-DBNAME = "news"
-try:
-  def get_articles():
+def connect(database_name="news"):
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error as e:
+        print "Unable to connect to database"
+        # THEN perhaps exit the program
+        sys.exit(1)
+        # OR perhaps throw an error
+        raise e
+
+       
+    
+
+def get_articles():
+  try:
     """Return the most popular three articles of all time from the 'database', most popular first."""
-    db = psycopg2.connect(database=DBNAME)
+    #db = psycopg2.connect(database="news")
     #print("successfully executed  step one")
+    db, c = connect()
     c = db.cursor()
     c.execute("select * from p_articles;")
     posts = c.fetchall()
     db.close()
-
+    
     print('the most popular three articles are')
     for a, b in posts:
       print  "the article:" ,a, " has --->", b ,"views"
     return posts
-except:
-  print("sorry it's a problem")
-get_articles()
+  except:
+    print("sorry it's a problem")
 
 
-try:
-  def get_authors():
+
+def get_authors():
+  try:
     """Return the most popular article authors of all time from the 'database', most popular first."""
-    db = psycopg2.connect(database=DBNAME)
+    #db = psycopg2.connect(database=DBNAME)
     #print("successfully executed  step one")
+    db, c = connect()
     c = db.cursor()
-    c.execute("select name, sum(count)from the_authors group by name order by sum desc ;")
+    c.execute("select * from p_authors ;")
     posts = c.fetchall()
     db.close()
-
+    
     print('--------------------------------------------')
     print('the most popular article authors are')
     for a, b in posts:
       print  "Mr.:" ,a, " has --->", b ,"views"
     return posts
-except:
-  print("sorry it's a problem")
-get_authors()
+  except:
+    print("sorry it's a problem")
 
-try:
-  def get_date():
+
+
+def get_date():
+  try:
     """Return days did more than 1% of requests lead to errors from the 'database'"""
-    db = psycopg2.connect(database=DBNAME)
+    #db = psycopg2.connect(database=DBNAME)
     #print("successfully executed  step one")
+    db, c = connect()
     c = db.cursor()
     c.execute("select to_char(date,'MonthDD,YYYY'),error_precentage from error_precentage where error_precentage>1;")
     posts = c.fetchall()
     db.close()
-
+    
     print('--------------------------------------------')
     print('the days whcih have request error more than 1%')
     for a, b in posts:
       print  "the day :" ,a, " has --->", b ,"%"
     return posts
-except:
-  print("sorry it's a problem")
-get_date()
+  except:
+    print("sorry it's a problem")
+
+
+
+
+if __name__ == '__main__':
+  get_articles()
+  get_authors()
+  get_date()
+
 
 
 
