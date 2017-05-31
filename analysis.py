973b42573a280dc5b13ab88e719c5e769d91b962
Ismail <Mohamed.Ahmed.Ismail@dnvgl.com>
2017-05-31 04:05:43 +0200

    the pep8 validation

--- analysis.py
+++ analysis.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
-import psycopg2, bleach
+import psycopg2
+import bleach
 import string
 
+
 def connect(database_name="news"):
-    """Connect to the PostgreSQL database.  Returns a database connection."""
     try:
+        """Connect to the PostgreSQL database"""
+        """Returns a database connection."""
         db = psycopg2.connect("dbname={}".format(database_name))
         c = db.cursor()
         return db, c
@@ -13,71 +16,63 @@ def connect(database_name="news"):
         print "Unable to connect to database"
         # THEN perhaps exit the program
         sys.exit(1)
-        
+
 
 def get_articles():
   try:
-    """Return the most popular three articles of all time from the 'database', most popular first."""
-   
+    """Return the most popular three articles of all time  """
+    """from the 'database', most popular first."""
     db, c = connect()
     c = db.cursor()
     c.execute("select * from p_articles;")
     posts = c.fetchall()
     db.close()
-    
     print('the most popular three articles are')
     for article, numView in posts:
-      print  "the article:" ,article, " has --->", numView ,"views"
+      print "the article:", article, " has ---> ", numView, "views "
     return posts
   except:
     print("sorry it's a problem")
 
 
-
 def get_authors():
   try:
-    """Return the most popular article authors of all time from the 'database', most popular first."""
+    """Return the most popular article authors of all time """
+    """from the 'database', most popular first."""
     db, c = connect()
     c = db.cursor()
     c.execute("select * from author_total_viewers ;")
     posts = c.fetchall()
     db.close()
-    
     print('--------------------------------------------')
     print('the most popular article authors are')
     for author, numView in posts:
-      print  "the author " ,author, " has --->", numView ,"views"
+      print "the author", author, "has --->", numView, "views"
     return posts
   except:
     print("sorry it's a problem")
-
 
 
 def get_date():
   try:
-    """Return days did more than 1% of requests lead to errors from the 'database'"""
+    """Return days did more than 1% of requests """
+    """lead to errors from the 'database'"""
     db, c = connect()
     c = db.cursor()
-    c.execute("select to_char(date,'MonthDD,YYYY'),error_precentage from error_precentage where error_precentage>1;")
+    c.execute("""select to_char(date,'MonthDD,YYYY'),
+               error_precentage from error_precentage
+               where error_precentage>1;""")
     posts = c.fetchall()
     db.close()
-    
     print('--------------------------------------------')
     print('the days whcih have request error more than 1%')
     for day, precent in posts:
-      print  "the day :" ,day, " has --->", precent ,"%"
+      print "the day :", day, " has --->", precent, "%"
     return posts
   except:
     print("sorry it's a problem")
-
-
-
 
 if __name__ == '__main__':
   get_articles()
   get_authors()
   get_date()
-
-
-
-
